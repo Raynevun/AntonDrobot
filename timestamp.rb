3fe03f81598eeca1476591e5b25ba2782afd00ad
AntonDrobot <4code4@gmail.com>
2015-06-04 00:15:18 +0300

    TimeStamp

--- timestamp.rb
+++ timestamp.rb
@@ -2,29 +2,49 @@ require 'net/http'
 require 'open-uri'
 require 'Nokogiri'
 
-uri = URI('https://beacon.nist.gov/rest/record/1433169120')
-params = { :limit => 10, :page => 3 }
-uri.query = URI.encode_www_form(params)
+def timeStampReq(record, time)
+	time_stamp_rec_type = record.to_s + (time - 60).to_s
+	uri = URI.join('https://beacon.nist.gov/rest/record/', time_stamp_rec_type)
 
-res = Net::HTTP.get_response(uri)
-doc = Nokogiri::XML(res.body)
+	params = { :limit => 10, :page => 3 }
+	uri.query = URI.encode_www_form(params)
 
-record = doc.at_xpath('//record')
+	res = Net::HTTP.get_response(uri)
+	doc = Nokogiri::XML(res.body)
 
-version = "Version: " + record.at_xpath('//version').content
-frequency = "Frequency: " + record.at_xpath('//frequency').content
-timeStamp = "TimeStamp: " + record.at_xpath('//timeStamp').content
-seedValue = "\n" + "SeedValue: " + record.at_xpath('//seedValue').content
-previousOutputValue = "\n" + "PreviousOutputValue: " + record.at_xpath('//previousOutputValue').content
-signatureValue = "\n" + "SignatureValue: " + record.at_xpath('//signatureValue').content
-outputValue = "\n" + "OutputValue: " + record.at_xpath('//outputValue').content
-statusCode = "StatusCode: " + record.at_xpath('//statusCode').content
+	record = doc.at_xpath('//record')
 
-puts version
-puts frequency
-puts timeStamp
-puts statusCode
-puts seedValue
-puts previousOutputValue
-puts signatureValue
-puts outputValue
+	version = record.at_xpath('//version').content.to_s
+	frequency = record.at_xpath('//frequency').content.to_s
+	timeStamp = record.at_xpath('//timeStamp').content.to_s
+	seedValue = record.at_xpath('//seedValue').content.to_s
+	previousOutputValue = record.at_xpath('//previousOutputValue').content.to_s
+	signatureValue = record.at_xpath('//signatureValue').content.to_s
+	outputValue = record.at_xpath('//outputValue').content.to_s
+	statusCode = record.at_xpath('//statusCode').content.to_s
+
+	data = Array.new
+	data.push version
+	data.push frequency
+	data.push timeStamp
+	data.push seedValue
+	data.push previousOutputValue
+	data.push signatureValue
+	data.push outputValue
+	data.push statusCode
+	
+return data
+end
+
+def PrintData()
+	data_arr = timeStampReq('next/' ,Time.now.to_i)
+
+	puts("Version: " + data_arr[0])
+	puts("Frequency: " + data_arr[1])
+	puts("TimeStamp: " + data_arr[2])
+	puts("\n" + "SeedValue: " + data_arr[3])
+	puts("\n" + "PreviousOutputValue: " + data_arr[4])
+	puts("\n" + "SignatureValue: " + data_arr[5])
+	puts("\n" + "OutputValue: " + data_arr[6])
+	puts("StatusCode: " + data_arr[7])
+end
